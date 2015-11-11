952ac246b6b08057d818a90d8e0baeeabef8edb9
Cody <Cody@75.102.87.42>
2015-11-11 15:09:57 -0500

    new Splash

--- src/team03app/SplashScreen.java
+++ src/team03app/SplashScreen.java
@@ -4,10 +4,39 @@
  */
 package team03app;
 
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import static javax.swing.WindowConstants.DISPOSE_ON_CLOSE;
+
 /**
  *
  * @author Cody
  */
-public class SplashScreen {
+public class SplashScreen extends JFrame {
+    private JPanel imagePanel = new JPanel();
+    private JLabel imageLabel = new JLabel();
+    private JLabel textLabel = new JLabel("Team 3 Group App");
     
+    private ImageIcon theSplashImage;
+    
+    public SplashScreen(){
+        
+        this.initCustomComponents();
+        this.setSize(400, 300);
+        this.setLocationRelativeTo(null);
+    }
+    
+    public void initCustomComponents(){
+        
+        theSplashImage = new ImageIcon("Splash.jpg");
+        imageLabel.setIcon(theSplashImage);
+        imagePanel.add(imageLabel);
+        imagePanel.add(textLabel);
+        this.getContentPane().add(imagePanel);
+        this.setResizable(false);
+        this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        
+    }
 }
