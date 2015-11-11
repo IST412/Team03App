36f58f2094a2d400c5cfdd55255db1f5c24c0fe7
Cody <Cody@75.102.87.42>
2015-11-11 15:09:37 -0500

    new UI

--- src/team03app/MainMenuUI.java
+++ src/team03app/MainMenuUI.java
@@ -4,10 +4,27 @@
  */
 package team03app;
 
+import java.awt.GridLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
 /**
  *
  * @author Cody
  */
-public class MainMenuUI {
+public class MainMenuUI extends JFrame{
+    
+    GridLayout MainMenuLayout = new GridLayout(0,2);
+    
+    public MainMenuUI(){
+        this.setSize(800, 600);
+        this.setLocationRelativeTo(null);
+        this.setResizable(false);
+        this.setLayout(MainMenuLayout);
+        this.add(new JButton("Social Zone"));
+        this.add(new JButton("Time Management Zone"));
+        this.add(new JButton("Assignment Zone"));
+        this.add(new JButton("Team Information Zone"));
+    }
     
 }
