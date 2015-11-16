65e5c4d162063f1e700924101d100dbeda59ba60
codyhubley <cwh5278@psu.edu>
2015-11-16 15:16:15 -0500

    Create TeamUI.java

---
+++ src/team03app/TeamUI.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package team03app;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Cody
+ */
+public class TeamUI extends JFrame{
+    
+    public TeamUI(){
+        this.setSize(800, 600);
+        this.setLocationRelativeTo(null);
+        this.setResizable(false);
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+    }
+    
+}
