9be8bbf660ea8adc3f900c9635809a5cd553fb35
codyhubley <cwh5278@psu.edu>
2015-11-16 15:18:31 -0500

    Create TimeCntl.java

---
+++ src/team03app/TimeCntl.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package team03app;
+
+/**
+ *
+ * @author Cody
+ */
+public class TimeCntl {
+    
+    public TimeCntl(MainMenuCntl theMainMenuCntl){
+        this.showTimeUI();
+        theMainMenuCntl.hideMain();
+    }
+    
+    public void showTimeUI(){
+        TimeUI theTimeUI = new TimeUI();
+        theTimeUI.setVisible(true);
+    }
+    
+}
