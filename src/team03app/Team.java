79ebbf26344ec9afd65efa63a887a5e671e15c3f
Marilynn Zayan <maz5156@psu.edu>
2015-11-27 14:54:03 -0500

    read in team information

--- src/team03app/Team.java
+++ src/team03app/Team.java
@@ -5,17 +5,32 @@
 package team03app;
 
 import java.io.File;
-import java.util.ArrayList;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Scanner;
-
 /**
  *
  * @author Cody
  */
 public class Team {
-    
+
+    File file = new File("TeamInfo.txt");
+
     public Team() {
+        try {
+            readInfo(file);
+        } catch (Exception ex) {
+            System.out.println("No file.");
+        }
+
+    }
+
+    public void readInfo(File file) throws FileNotFoundException, IOException {
+        Scanner in = new Scanner(new FileReader(file));
+        while(in.hasNext()) {
+            System.out.println(in.nextLine());
+        }
         
-                
     }
 }
