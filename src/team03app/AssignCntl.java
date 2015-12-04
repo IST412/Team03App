5affc005e6265a3f8435c38bda4a37e666d16ad7
eos5252 <eos5252@psu.edu>
2015-12-04 16:28:54 -0500

    Update AssignCntl.java
    added back button

--- src/team03app/AssignCntl.java
+++ src/team03app/AssignCntl.java
@@ -1,23 +1,24 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package team03app;
 
-/**
- *
- * @author Cody
- */
 public class AssignCntl {
-    
+        public final MainMenuCntl theMainMenuCntl;
+
     public AssignCntl(MainMenuCntl theMainMenuCntl){
+        this.theMainMenuCntl = theMainMenuCntl;
         this.showAssignUI();
         theMainMenuCntl.hideMain();
     }
     
     public void showAssignUI(){
-        AssignUI theAssignUI = new AssignUI();
+        AssignUI theAssignUI = new AssignUI( this);
         theAssignUI.setVisible(true);
     }
+    public void hideAssignUI() {
+        AssignUI theAssignUI = new AssignUI(this);
+        theAssignUI.setVisible(false);
+    }
     
+    public void showMain(){
+        theMainMenuCntl.showMain();
+    } 
 }
