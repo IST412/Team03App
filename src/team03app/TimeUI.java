a34dfa005219e321bc1b536e4fc0d26cbde2752b
Marilynn Zayan <maz5156@psu.edu>
2015-12-04 16:38:20 -0500

    Revert "Revert "Revert "Revert "Revert "Update TimeUI.java"""""
    This reverts commit bf3cd2be1b522f77b06f4229a84b687af1fe599c.

--- src/team03app/TimeUI.java
+++ src/team03app/TimeUI.java
@@ -1,37 +1,22 @@
-
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package team03app;
 
-import java.awt.event.*;
-import javax.swing.*;
+import javax.swing.JFrame;
 
-public class TimeUI extends JFrame implements ActionListener {
- 
-    private final JButton back;
-    private final Time time;
-    private final TimeCntl theTimeCntl;
+/**
+ *
+ * @author Cody
+ */
+public class TimeUI extends JFrame{
     
-    public TimeUI(TimeCntl theTimeCntl){
-        this.theTimeCntl = theTimeCntl;
-        this.time = new Time();
+    public TimeUI(){
         this.setSize(800, 600);
         this.setLocationRelativeTo(null);
         this.setResizable(false);
-        this.setLayout(null);
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-
-        back = new JButton("< Back");
-        back.setBounds(50, 50, 100, 50);
-        back.addActionListener(this);
-        this.add(back);
-        
-
     }
     
-    public void actionPerformed(ActionEvent e){
-      String action = e.getActionCommand();
-        if (action.equals("< Back")) {
-            theTimeCntl.hideTimeUI();
-            theTimeCntl.theMainMenuCntl.showMain();
-        }  
-    }
 }
