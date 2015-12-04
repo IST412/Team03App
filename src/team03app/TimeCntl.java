c0d2f73f309ec110c7abbe8a0d2b7826e5d80751
eos5252 <eos5252@psu.edu>
2015-12-04 16:30:26 -0500

    Update TimeCntl.java

--- src/team03app/TimeCntl.java
+++ src/team03app/TimeCntl.java
@@ -1,23 +1,26 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package team03app;
 
-/**
- *
- * @author Cody
- */
 public final class TimeCntl {
-    
+    public final MainMenuCntl theMainMenuCntl;
+
     public TimeCntl(MainMenuCntl theMainMenuCntl){
+        this.theMainMenuCntl = theMainMenuCntl;
         this.showTimeUI();
         theMainMenuCntl.hideMain();
     }
     
     public void showTimeUI(){
-        TimeUI theTimeUI = new TimeUI();
+        TimeUI theTimeUI = new TimeUI(this);
         theTimeUI.setVisible(true);
+    }
+    public void hideTimeUI(){
+        TimeUI theTimeUI = new TimeUI(this);
+        theTimeUI.setVisible(false);
+    }
+    
+    public void showMain() {
+        theMainMenuCntl.showMain();
     }
     
 }
