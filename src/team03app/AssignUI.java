2134d25ff3f8913b910163707fd813ba78ab0f96
eos5252 <eos5252@psu.edu>
2015-12-04 16:29:29 -0500

    Update AssignUI.java
    added back button!!!!!!

--- src/team03app/AssignUI.java
+++ src/team03app/AssignUI.java
@@ -1,22 +1,35 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package team03app;
 
-import javax.swing.JFrame;
+import java.awt.event.*;
+import javax.swing.*;
 
-/**
- *
- * @author Cody
- */
-public class AssignUI extends JFrame{
-    
-    public AssignUI(){
+public class AssignUI extends JFrame implements ActionListener {
+
+    private final JButton back;
+    private final JTextArea information = new JTextArea("");
+    private final Assign assign;
+    private final AssignCntl theAssignCntl;
+
+    public AssignUI(AssignCntl theAssignCntl) {
+        this.theAssignCntl = theAssignCntl;
+        this.assign = new Assign();
         this.setSize(800, 600);
         this.setLocationRelativeTo(null);
         this.setResizable(false);
+        this.setLayout(null);
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        back = new JButton("< Back");
+        back.setBounds(50, 50, 100, 50);
+        back.addActionListener(this);
+        this.add(back);
     }
-    
+
+    public void actionPerformed(ActionEvent e) {
+        String action = e.getActionCommand();
+        if (action.equals("< Back")) {
+            theAssignCntl.hideAssignUI();
+            theAssignCntl.theMainMenuCntl.showMain();
+        }
+    }
 }
