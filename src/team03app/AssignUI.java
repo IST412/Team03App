abe78800c7738c93b0bc82317843b2bcc40b79e0
eos5252 <eos5252@psu.edu>
2015-12-07 05:29:59 -0500

    Update AssignUI.java
    added background to assignUI

--- src/team03app/AssignUI.java
+++ src/team03app/AssignUI.java
@@ -5,24 +5,42 @@ import javax.swing.*;
 
 public class AssignUI extends JFrame implements ActionListener {
 
-    private final JButton back;
-    private final JTextArea information = new JTextArea("");
+    private final JButton back, toDo, feedback, jUnit;
+    private final JTextArea assignInfo = new JTextArea("");
     private final Assign assign;
     private final AssignCntl theAssignCntl;
+    JPanel toDoPanel;
 
     public AssignUI(AssignCntl theAssignCntl) {
         this.theAssignCntl = theAssignCntl;
         this.assign = new Assign();
         this.setSize(800, 600);
         this.setLocationRelativeTo(null);
-        this.setResizable(false);
-        this.setLayout(null);
+        //this.setResizable(false);
+        //this.setLayout(null);
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         back = new JButton("< Back");
-        back.setBounds(50, 50, 100, 50);
+        back.setBounds(50, 500, 100, 50);
         back.addActionListener(this);
         this.add(back);
+        
+        feedback = new JButton("Feedback");
+        feedback.setBounds(250, 500, 100, 50);
+        feedback.addActionListener(this);
+        this.add(feedback);
+        
+        jUnit = new JButton("JUnit Test");
+        jUnit.setBounds(350, 500, 100, 50);
+        jUnit.addActionListener(this);
+        this.add(jUnit);
+        
+        for (String info : assign.assignments) {
+            assignInfo.append(info + "\n");
+        }
+        assignInfo.setOpaque(false);
+        //assignInfo.setBounds(250, 150, 350, 300);
+        this.add(assignInfo); 
     }
 
     public void actionPerformed(ActionEvent e) {
@@ -31,5 +49,6 @@ public class AssignUI extends JFrame implements ActionListener {
             theAssignCntl.hideAssignUI();
             theAssignCntl.theMainMenuCntl.showMain();
         }
-    }
-}
+       
+    }}
+  
