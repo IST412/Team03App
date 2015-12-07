d48625cb45c88fc7024d484644021afe785fbefd
eos5252 <eos5252@psu.edu>
2015-12-07 05:39:52 -0500

    Update Assign.java

--- src/team03app/Assign.java
+++ src/team03app/Assign.java
@@ -1,10 +1,25 @@
 package team03app;
-
+import java.io.*;
+import java.util.*;
 
 public final class Assign {
     
+     private final File file = new File("Assign Fourm.txt");
+    public final ArrayList<String> assignments = new ArrayList<>();
+
     public Assign() {
-        
+        try {
+            readInfo(file);
+        } catch (Exception ex) {
+            System.out.println("No file.");
+        }
     }
+
+    public void readInfo(File file) throws FileNotFoundException, IOException {
+        Scanner in = new Scanner(new FileReader(file));
+        while(in.hasNext()) {
+            assignments.add(in.nextLine());
+        }
     }
+}
 
